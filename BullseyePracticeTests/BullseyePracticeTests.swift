ae3fce187e8e297b3a9c8a8d7bd8ea03b2b99b17
Gred <jihong.park.dev@gmail.com>
2019-12-18 18:39:16 +0900

    Initial Commit

---
+++ BullseyePracticeTests/BullseyePracticeTests.swift
@@ -0,0 +1,34 @@
+//
+//  BullseyePracticeTests.swift
+//  BullseyePracticeTests
+//
+//  Created by 박지홍 on 2019/12/18.
+//  Copyright © 2019 Jihong Park. All rights reserved.
+//
+
+import XCTest
+@testable import BullseyePractice
+
+class BullseyePracticeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
