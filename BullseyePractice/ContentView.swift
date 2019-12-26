1e00da977bba63d5108efcb948cdadb28725b4ef
Gred <jihong.park.dev@gmail.com>
2019-12-26 20:35:23 +0900

    Create Bullseye UI with swiftUI

--- BullseyePractice/ContentView.swift
+++ BullseyePractice/ContentView.swift
@@ -10,29 +10,59 @@ import SwiftUI
 
 struct ContentView: View {
     @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
+    
     var body: some View {
         VStack {
-            VStack {
-                Text("Welcome to the my first app")
-                    .fontWeight(.semibold)
-                    .foregroundColor(Color.green)
-                Button(action: {
-                    print("Button Pressed!")
-                    self.alertIsVisible = true
-                }) {
-                    Text("Hit me!")
-                }
-                .alert(isPresented: self.$alertIsVisible) {
-                    Alert(title: Text("Hello, there!"), message: Text("This is my first popup."), dismissButton: .default(Text("Awesome!")))
-                }
-
+            // Target row
+            HStack {
+                Text("Put the bullseye as close as you can to :")
+                Text("100")
             }
+            Spacer()
+            
+            // Slider row
+            HStack {
+                Text("1")
+                Slider(value: self.$sliderValue, in: 0...100)
+                Text("100")
+            }
+            Spacer()
+            Button(action: {
+                print("Button Pressed!")
+                self.alertIsVisible = true
+            }) {
+                Text("Hit me!")
+            }
+            .alert(isPresented: self.$alertIsVisible) {
+                Alert(title: Text("Hello, there!"), message: Text("The slider value is \(self.sliderValue)"), dismissButton: .default(Text("Awesome!")))
+            }
+            
+            Spacer()
+            HStack {
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("Start Over")
+                    
+                }
+                Spacer()
+                Text("Score: ")
+                Text("9999")
+                Spacer()
+                Text("Round: ")
+                Text("1")
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("Info")
+                    
+                }
+            }
+            .padding(.bottom, 10)
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 896, height: 414))
     }
 }
