d530802ae256ee54e5946ece3d943909ab3cca5f
Gred <jihong.park.dev@gmail.com>
2020-01-24 12:18:00 +0900

    BE with UIkit, Swift4

---
+++ BE_UIKit/BE_UIKit/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  BE_UIKit
+//
+//  Created by 박지홍 on 2020/01/24.
+//  Copyright © 2020 gred. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showAlert() {
+        print("hello")
+    }
+}
+
