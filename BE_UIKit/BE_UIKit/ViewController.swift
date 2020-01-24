452ef9a1d5484a9f2d5e2636930c8be70b003514
Gred <jihong.park.dev@gmail.com>
2020-01-24 15:22:46 +0900

    binding label and slider make method

--- BE_UIKit/BE_UIKit/ViewController.swift
+++ BE_UIKit/BE_UIKit/ViewController.swift
@@ -9,26 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        currentValue = Int(slider.value.rounded())
+        startNewRound()
     }
 
     @IBAction func showAlert() {
-        let alertController = UIAlertController(title: "Hello,swift!", message: "This is a button", preferredStyle: .alert)
+        let message = "Your score is \(currentValue)" + "\n The targetValue is \(targetValue)"
+        let alertController = UIAlertController(title: "Hello,swift!", message: message, preferredStyle: .alert)
         let alertAction = UIAlertAction(title: "Good good.", style: .default, handler: nil)
         
         alertController.addAction(alertAction)
         present(alertController, animated: true, completion: nil)
+        
+        startNewRound()
     }
     
-    @IBAction func knockingAlert() {
-        let alertController = UIAlertController(title: "Twice!", message: "No sana, No life,", preferredStyle: .alert)
-        let alertAction = UIAlertAction(title: "Good good.", style: .cancel, handler: nil)
-        
-        alertController.addAction(alertAction)
-        present(alertController, animated: true, completion: nil)
+    @IBAction func showSliderValue (_ slider:UISlider) {
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
+    func updateLabel() {
+        targetLabel.text = String(targetValue)
     }
 }
 
