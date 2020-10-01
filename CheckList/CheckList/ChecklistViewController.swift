1921a9bb095557dd5181270a9631ad766eefc3f8
Gred <jihong.park.dev@gmail.com>
2020-10-01 18:06:35 +0900

    checklist init

---
+++ CheckList/CheckList/ChecklistViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckList
+//
+//  Created by Ji Hong Park on 2020/10/01.
+//  Copyright © 2020 Ji Hong Park. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
